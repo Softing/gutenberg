797a02948aa0154bd018a03d9570cb0273b53fa8
Ilya Skorik <ilya@skorik.me>
2010-12-17 10:28:02 +0000

    No comments

--- lib/Inprint/Fascicle/Images.pm
+++ lib/Inprint/Fascicle/Images.pm
@@ -99,9 +99,11 @@ sub view {
         
         my $font;
         my $fontsize;
+        my $fontheight;
         if ($^O eq "MSWin32") {
             $font = 'C:\Windows\Fonts\ARIALUNI.TTF';
-            $fontsize = 10;
+            $fontsize = 8;
+            $fontheight = 5;
         }
         
         if ($^O eq "linux") {
@@ -118,95 +120,17 @@ sub view {
         $wrapbox->set_font(gdMediumBoldFont);
         $wrapbox->set_font($font, $fontsize);
         $wrapbox->set(align => 'center', width => $x2-$x1);
-        $wrapbox->draw( $x1, (($y2-$y1)/2)+$y1-10);
+        
+        my $txty = ($y2-$y1)/2;
+        $txty = $y1+$txty - $fontheight;
+        
+        $wrapbox->draw( $x1, $txty);
         
     }
     
     $c->tx->res->headers->content_type('image/png');
     $c->render_data($img->png);
 
-}
-
-sub draw_page {
-    
-    my $c = shift;
-    
-    my $img          = shift;
-    my $grid_wh      = shift;
-    my $grid_xy      = shift;
-    my $displacement = shift;
-    my $modules      = shift;
-    
-    my ($grid_w, $grid_h) = split "x", $grid_wh;
-    my ($grid_x, $grid_y) = split "x", $grid_xy;
-    
-    $img->bgcolor("silver");
-    $img->fgcolor("silver");
-    
-    my $pen_size = 1;
-    my $line_height = ceil($pen_size/2);
-    
-    $img->penSize($pen_size);
-    
-    #$img->moveTo(0+$displacement,0);
-    #$img->lineTo($displacement,$grid_h-$line_height);
-    #$img->moveTo($grid_w+$displacement-1,0);
-    #$img->lineTo($grid_w+$displacement-1,$grid_h-$line_height);
-    #
-    #$img->moveTo(0+$displacement, 0);
-    #$img->lineTo($grid_w+$displacement-$line_height,0);
-    #$img->moveTo(0+$displacement,$grid_h-$line_height);
-    #$img->lineTo($grid_w+$displacement-$line_height,$grid_h-$line_height);
-    
-    if ($grid_y < 10) {
-        for (my $c=0;$c<$grid_y;$c++) {
-            my $px = $c * ($grid_h/$grid_y);
-            $img->moveTo(0+$displacement,$px);
-            $img->lineTo($grid_w+$displacement-$line_height, $px);
-        }
-    }
-    
-    if ($grid_x < 10 ) {
-        for (my $c=0;$c<$grid_x;$c++) {
-            my $px = $c * ($grid_w/$grid_x);
-            $img->moveTo($px+$displacement,0);
-            $img->lineTo($px+$displacement,$grid_h-$line_height);
-        }
-    }
-    
-    my $block_pen_size = 2;
-    my $block_line_height = ceil($block_pen_size / 2);
-    
-    my $block_w = ($grid_w/$grid_x);
-    my $block_h = ($grid_h/$grid_y);
-    
-    foreach my $block (@$modules) {
-        
-        $img->penSize($block_pen_size);
-        
-        my $x = $$block[0];
-        my $y = $$block[1];
-        
-        my $w = $$block[2];
-        my $h = $$block[3];
-        
-        my $bl_posx1 = ( $x-1 ) * $block_w;
-        my $bl_posy1 = ( $y-1 ) * $block_h;
-        
-        my $bl_posx2 = ( $w * $block_w ) + $bl_posx1;
-        my $bl_posy2 = ( $h * $block_h ) + $bl_posy1;
-        
-        my $bgcolor = $$block[4];
-        my $fgcolor = $$block[5];
-        
-        $img->bgcolor($bgcolor);
-        $img->fgcolor($fgcolor);
-        
-        $img->rectangle($bl_posx1 + $displacement +2, $bl_posy1 + 2, $bl_posx2 + $displacement - 2, $bl_posy2 - 2);
-        
-    }
-    
-    return $img;
 }
 
 1;
