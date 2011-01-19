c2202d82aff15793f7c4c1c11a8db4d3a7435816
Ilya Skorik <ilya@skorik.me>
2011-01-19 11:08:40 +0000

    No comments

--- lib/Inprint/Calendar.pm
+++ lib/Inprint/Calendar.pm
@@ -39,6 +39,12 @@ sub tree {
             WHERE
                 id <> '00000000-0000-0000-0000-000000000000'
                 AND subpath(path, nlevel(path) - 2, 1)::text = replace(?, '-', '')::text
+                AND EXISTS(
+                    SELECT true
+                    FROM cache_access access
+                    WHERE access.path @> editions.path AND access.type = 'editions'
+                        AND ('editions.calendar.manage' = ANY(access.terms) OR 'editions.calendar.view' = ANY(access.terms) )
+                )
         ";
         push @data, $i_node;
         push @data, $i_node;
