c9e447905b5ca495ea7c8cb947178bdae2a6ef5b
Ilya Skorik <ilya@skorik.me>
2011-02-21 12:58:57 +0000

    Добавлена поддержка expires +10 days

--- dev/installation/ubuntu-10.04/install-libs.sh
+++ dev/installation/ubuntu-10.04/install-libs.sh
@@ -1,20 +1,30 @@
-apt-get install build-essential
-apt-get install libdevel-simpletrace-perl
-apt-get install libtest-mockmodule-perl
-apt-get install libconfig-tiny-perl
-apt-get install libdbd-pg-perl
-apt-get install libyaml-perl
-apt-get install libossp-uuid-perl
-apt-get install libfile-copy-recursive-perl
-apt-get install libwww-perl
-apt-get install libhtml-scrubber-perl
-apt-get install perlmagick
-apt-get install libgd-gd2-perl
-apt-get install libgd-text-perl
-apt-get install sun-java6-jre
-apt-get install imagemagick
-apt-get install ghostscript
+apt-get -y install build-essential
+apt-get -y install libdevel-simpletrace-perl
+apt-get -y install libtest-mockmodule-perl
+apt-get -y install libconfig-tiny-perl
+apt-get -y install libdbd-pg-perl
+apt-get -y install libyaml-perl
+apt-get -y install libossp-uuid-perl
+apt-get -y install libfile-copy-recursive-perl
+apt-get -y install libwww-perl
+apt-get -y install libhtml-scrubber-perl
+apt-get -y install perlmagick
+apt-get -y install libgd-gd2-perl
+apt-get -y install libgd-text-perl
+apt-get -y install sun-java6-jre
+apt-get -y install imagemagick
+apt-get -y install ghostscript
+apt-get -y install postgresql
+apt-get -y install postgresql-contrib
+apt-get -y install apache2
+
+a2enmod rewrite
+a2enmod expires
+
+/etc/init.d/apache2 restart
+/etc/init.d/postgresql-8.4 restart
 
 perl -MCPAN -e 'install DBIx::Connector' 
 perl -MCPAN -e 'install IO::Epoll'
 perl -MCPAN -e 'install File::Util'
+perl -MCPAN -e 'install Mojolicious'
