3a026f7119c448391a32c2e73bc7c8c2ad8520d5
Ilya Skorik <ilya@skorik.me>
2011-03-21 19:47:29 +0000

    Исправлен баг с номерами полос

---
+++ lib/Inprint/Fascicle/Utils.pm
@@ -0,0 +1,150 @@
+package Inprint::Fascicle::Utils;
+
+# Inprint Content 5.0
+# Copyright(c) 2001-2011, Softing, LLC.
+# licensing@softing.ru
+# http://softing.ru/license
+
+use strict;
+use warnings;
+
+use base 'Inprint::BaseController';
+
+sub updateDocumentsPagesCache {
+    my ($c, $fascicle) = @_;
+
+    $c->sql->Do("
+            UPDATE documents SET
+                pages = array_to_string(
+                    ARRAY( SELECT seqnum FROM fascicles_pages as pages, fascicles_map_documents as mapping
+                        WHERE mapping.page = pages.id AND mapping.entity = documents.id ORDER BY pages.seqnum )
+                    , ','),
+                firstpage = ( SELECT min(seqnum) FROM fascicles_pages as pages, fascicles_map_documents as mapping
+                        WHERE mapping.page = pages.id AND mapping.entity = documents.id
+                )
+            WHERE fascicle = ?
+        ", [ $fascicle ]);
+
+    return 1;
+}
+
+
+sub compressString {
+
+    my $c = shift;
+    my $string = shift;
+
+    my @pages = split(/[^\d]+/, $string );
+    my @string;
+    for ( my $i = 0; $i <= $#pages; $i++ ) {
+
+        my $cp = int( $pages[$i] );
+        my $pp = int( $pages[$i-1] );
+        my $fp = int( $pages[$i+1] );
+
+        next unless $cp;
+
+        if ( !$pp ) {
+                push @string, $cp;
+        } elsif (!$fp ) {
+                push @string, $cp;
+        } elsif ( $pp && $fp && $cp-1 == $pp && $cp+1 == $fp ) {
+                push @string, "-";
+        } else {
+                push @string, $cp;
+        }
+    };
+
+    $string = join (',',@string);
+    $string =~ s/,-,/-/g;
+    $string =~ s/-,/-/g;
+    $string =~ s/-+/-/g;
+    $string =~ s/,/, /g;
+
+    return $string;
+}
+
+sub uncompressString {
+
+    my $c = shift;
+    my $string = shift;
+
+    my @pairs = split '[^\d|\-|\:|<|>]', $string;
+
+    my @source;
+
+    foreach my $pair ( @pairs ) {
+
+        next unless $pair;
+
+        if ( $pair =~ /\-/ ) {
+            my ($start, $end) = split '-', $pair;
+            my $array = [];
+            for ($start .. $end) {
+                push @source, $_ if ($_>0);
+            }
+        } elsif ( $pair =~ /</ ) {
+            my ($count, $after) = split '<', $pair;
+            for ( $after-$count .. $after) {
+                push @source, $_ if ($_>0);
+            }
+        } elsif ( $pair =~ />/ ) {
+            my ($count, $after) = split '>', $pair;
+            for ( $after .. $after+$count ) {
+                push @source, $_ if ($_>0);
+            }
+        } elsif ( $pair =~ /\:/ ) {
+            my ($count, $after) = split ':', $pair;
+            for ($after+1 .. $after+$count) {
+                push @source, $_ if ($_>0);
+            }
+        } elsif ( $pair =~ /\d/ ) {
+            push @source, $pair;
+        }
+    }
+
+    my @pages;
+
+    my %seen = (); foreach my $item (@source) {    push(@pages, $item) unless $seen{$item}++; }
+
+    @pages = sort { $a <=> $b } @pages;
+
+    return \@pages;
+}
+
+sub getChunks {
+    my $c = shift;
+    my $pages = shift;
+
+    my @result;
+
+    for (my $c=0; $c <= $#{$pages}; $c++ ) {
+
+        my $prev = $$pages[$c-1] || undef;
+        my $curr = $$pages[$c] || undef;
+        my $next = $$pages[$c+1] || undef;
+
+        #die "$prev $curr $next";
+
+        if ($c == 0) {
+            push @result, [ $curr ];
+            next;
+        }
+
+        if ($prev && $curr-$prev == 1 ) {
+            push @{ $result[-1] }, $curr ;
+            next;
+        }
+
+        if ($prev && $curr-$prev > 1 ) {
+            push @result, [ $curr ];
+            next;
+        }
+
+
+    }
+
+    return \@result;
+}
+
+1;
