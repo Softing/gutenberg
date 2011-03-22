8f8a373e39cc021e3068309a5295fa27d2c5eb30
Ilya Skorik <ilya@skorik.me>
2011-03-22 10:53:50 +0000

    Обновил скрипт запуска OO, добавил PID файл

--- dev/installation/jod-webapp/start-daemon.sh
+++ dev/installation/jod-webapp/start-daemon.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-java -jar jetty-runner-7.2.0.v20101020.jar --port 9999  --log daemon.log softing-filebackend-service-1.3.war
+
+PID_FILE=/var/run/inprint-converter.pid
+
+export INPRINT_CONVERTER_HOME=/root/jod-webapp
+
+case "$1" in
+    start)
+        cd ${INPRINT_CONVERTER_HOME} || exit
+        java -Xmx256m -Xms256m -jar jetty-runner-7.2.0.v20101020.jar --port 9999 --log daemon.log softing-filebackend-service-1.3.war &
+        echo $! > $PID_FILE
+        ;;
+    stop)
+        pid=`cat $PID_FILE`
+        kill $pid
+        rm $PID_FILE
+        ;;
+esac
+exit 0
+
