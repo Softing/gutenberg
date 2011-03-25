89c13e9f36e279ad421eceebd2783aa4afd50a42
Ilya Skorik <ilya@skorik.me>
2011-03-25 12:44:41 +0000

    Обновлен модуль загрузки файлов

--- lib/Inprint/Downloads.pm
+++ lib/Inprint/Downloads.pm
@@ -70,7 +70,7 @@ sub list {
         $sql.= " AND (". join(" OR ", @filter) .") ";
     }
 
-    $sql .= " ORDER BY fls.updated, fls.file_path ";
+    $sql .= " ORDER BY fls.updated ASC, fls.file_path ";
     $result = $c->sql->Q($sql, \@params)->Hashes;
 
     $c->smart_render(\@errors, $result);
