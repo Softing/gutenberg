d064e9f5e4745b53b8c22c7521d2c831b0add156
Ilya Skorik <ilya@skorik.me>
2011-03-25 16:55:26 +0000

    Обновлен модуль загрузки файлов, исправления

--- public/widgets/modules/documents/downloads/_panels-access.js
+++
@@ -1,7 +0,0 @@
-Inprint.documents.downloads.Access = function(parent, panels) {
-
-    var profile = panels.profile;
-
-    var oid = parent.oid;
-
-};
--- public/widgets/modules/documents/downloads/_panels-interaction.js
+++
@@ -1,7 +0,0 @@
-Inprint.documents.downloads.Interaction = function(parent, panels) {
-
-    parent.getSelectionModel().on("selectionchange", function(sm, node) {
-        
-    });
-
-};
