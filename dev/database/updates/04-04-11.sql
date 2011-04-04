09a8e083afc60c5a43126a49ece32a91082f37a4
Ilya Skorik <ilya@skorik.me>
2011-04-04 12:16:14 +0000

    Исправления прав, убрал кеш из основных функций

--- dev/database/updates/04-04-11.sql
+++ dev/database/updates/04-04-11.sql
@@ -5,3 +5,49 @@ SELECT rules.section ||'.'|| rules.subsection ||'.'|| rules.term || ':' || mappe
 FROM map_member_to_rule as mapper, rules WHERE mapper.term = rules.id AND mapper.id = map_member_to_rule.id);
 
 ALTER TABLE map_member_to_rule ALTER COLUMN termkey SET NOT NULL;
+
+--
+
+ALTER TABLE fascicles DROP COLUMN pnum;
+
+ALTER TABLE fascicles ADD COLUMN num integer;
+UPDATE fascicles SET num = 0;
+ALTER TABLE fascicles ALTER COLUMN num SET NOT NULL;
+ALTER TABLE fascicles ALTER COLUMN num SET DEFAULT 0;
+
+--
+
+ALTER TABLE fascicles DROP COLUMN anum;
+
+ALTER TABLE fascicles ADD COLUMN anum integer;
+UPDATE fascicles SET anum = 0;
+ALTER TABLE fascicles ALTER COLUMN anum SET NOT NULL;
+ALTER TABLE fascicles ALTER COLUMN anum SET DEFAULT 0;
+
+--
+
+ALTER TABLE fascicles ADD COLUMN deleted boolean;
+ALTER TABLE fascicles ALTER COLUMN deleted SET DEFAULT false;
+
+--
+
+ALTER TABLE fascicles DROP COLUMN flagadv;
+
+ALTER TABLE fascicles ADD COLUMN adv_enabled boolean;
+UPDATE fascicles SET adv_enabled = true;
+ALTER TABLE fascicles ALTER COLUMN adv_enabled SET DEFAULT false;
+
+--
+
+ALTER TABLE fascicles DROP COLUMN flagdoc;
+
+ALTER TABLE fascicles ADD COLUMN doc_enabled boolean;
+UPDATE fascicles SET doc_enabled = true;
+ALTER TABLE fascicles ALTER COLUMN doc_enabled SET DEFAULT false;
+ALTER TABLE fascicles ALTER COLUMN doc_enabled SET NOT NULL;
+--
+
+ALTER TABLE fascicles RENAME COLUMN datedoc   TO doc_date;
+ALTER TABLE fascicles RENAME COLUMN dateadv   TO adv_date;
+ALTER TABLE fascicles RENAME COLUMN dateprint TO print_date;
+ALTER TABLE fascicles RENAME COLUMN dateout   TO release_date;
