1fc9ecbbe07e16c2b13a08fa8fbaf89400d0f1ee
Ilya Skorik <ilya@skorik.me>
2011-05-12 07:33:04 +0000

    Опечатки

--- dev/database/updates/11-05-11.sql
+++ dev/database/updates/11-05-11.sql
@@ -8,10 +8,10 @@ ALTER TABLE fascicles_modules RENAME COLUMN check_height TO height;
 ALTER TABLE fascicles_modules RENAME COLUMN check_width_float TO fwidth;
 ALTER TABLE fascicles_modules RENAME COLUMN check_height_float TO fheight;
 
-//ALTER TABLE fascicles_requests ADD COLUMN origin_width   real DEFAULT 0 NOT NULL;
-//ALTER TABLE fascicles_requests ADD COLUMN origin_height  real DEFAULT 0 NOT NULL;
-//ALTER TABLE fascicles_requests ADD COLUMN origin_fwidth  real DEFAULT 0 NOT NULL;
-//ALTER TABLE fascicles_requests ADD COLUMN origin_fheight real DEFAULT 0 NOT NULL;
+--ALTER TABLE fascicles_requests ADD COLUMN origin_width   real DEFAULT 0 NOT NULL;
+--ALTER TABLE fascicles_requests ADD COLUMN origin_height  real DEFAULT 0 NOT NULL;
+--ALTER TABLE fascicles_requests ADD COLUMN origin_fwidth  real DEFAULT 0 NOT NULL;
+--ALTER TABLE fascicles_requests ADD COLUMN origin_fheight real DEFAULT 0 NOT NULL;
 
 ALTER TABLE fascicles_tmpl_modules ADD COLUMN width   real DEFAULT 0 NOT NULL;
 ALTER TABLE fascicles_tmpl_modules ADD COLUMN height  real DEFAULT 0 NOT NULL;
