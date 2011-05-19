077a6efbbfd9fb61e71707174f0422d0c9f4aa7a
Ilya Skorik <ilya@skorik.me>
2011-05-19 00:16:49 +0000

    Обновил поддержку Windows

--- dev/installation/windows7/install-libs.bat
+++ dev/installation/windows7/install-libs.bat
@@ -1,11 +1,15 @@
-perl -MCPAN -e "install DBIx::Connector"
-perl -MCPAN -e "install File::Util"
+:: Core
 perl -MCPAN -e "install Mojolicious"
 perl -MCPAN -e "install Devel::SimpleTrace"
+perl -MCPAN -e "install DBIx::Connector"
 perl -MCPAN -e "install Config::Tiny"
 perl -MCPAN -e "install Data::UUID"
+
+:: Otger
+perl -MCPAN -e "install File::Util"
 perl -MCPAN -e "install File::Copy::Recursive"
 perl -MCPAN -e "install HTML::Scrubber"
 perl -MCPAN -e "install GD::Text"
+perl -MCPAN -e "install Text::Unidecode"
 perl -MCPAN -e "install Image::Magick"
-perl -MCPAN -e "install Convert::Cyrillic"
+
