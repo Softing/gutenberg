2a09339a4aa13fac6bf9565fb55a796e6f4cfe1d
Ilya Skorik <ilya@skorik.me>
2011-08-10 06:21:20 +0000

    Добавил патч для БД, который удаляет пустые и ненужные таблицы

---
+++ dev/database/updates/2011-08-09.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS rss;
+DROP TABLE IF EXISTS rss_feeds;
+DROP TABLE IF EXISTS rss_feeds_mapping;
+DROP TABLE IF EXISTS rss_feeds_options;
+
+DROP TABLE IF EXISTS map_role_to_rule;
+DROP TABLE IF EXISTS roles;
+
+DROP TABLE IF EXISTS migration;
+DROP TABLE IF EXISTS editions_options;
+
+DROP TABLE IF EXISTS cache_versions;
+DROP TABLE IF EXISTS ad_requests;
+DROP TABLE IF EXISTS fascicles_requests_files;
