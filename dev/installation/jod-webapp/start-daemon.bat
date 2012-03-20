765c367a1d1bc5926f9d3b8dc20342232167f0c4
Ilya Skorik <ilya@skorik.me>
2012-03-20 20:11:05 +0000

    Обновил движок конвертирования документов

--- dev/installation/jod-webapp/start-daemon.bat
+++ dev/installation/jod-webapp/start-daemon.bat
@@ -1,5 +1,6 @@
-set VERSION=1.3
+set VERSION=1.6
 set JAVAPATH="C:\Program Files (x86)\Java\jre6\bin\java"
 
 taskkill /F /T /IM soffice.bin
-%JAVAPATH% -jar jetty-runner-7.2.0.v20101020.jar --port 9999  --log daemon.log softing-filebackend-service-%VERSION%.war
+::-Doffice.home="C:/Program Files (x86)/LibreOffice 3.5/"
+%JAVAPATH% -jar jetty-runner-7.2.0.v20101020.jar --port 9999 --log daemon.log softing-filebackend-service-%VERSION%.war
